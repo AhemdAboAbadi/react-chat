bbdf0a67984f5dae2b5a0958552caf2278c34414
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-11-23 14:48:30 +0200

    finish initial socket.io and start building Chat component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import Chat from './Chat';
 
 const socket = io.connect('http://localhost:3000');
 
@@ -8,21 +9,31 @@ function App() {
   const [userName, setUserName] = useState('');
   const [room, setRoom] = useState('');
 
-
   const joinRoom = () => {
     if (userName !== '' && room !== '') {
-      
+      socket.emit('joinRoom', room);
     }
-  }
-
-
+  };
 
   return (
     <div className="App">
       <h3>Ahmed Chat</h3>
-      <input type="text" placeholder="Hi Ahmed " onChange={(e) => {setUserName(e.target.value)}}/>
-      <input type="text" placeholder="Room ID ... "  onChange={(e) => {setRoom(e.target.value)}}/>
-      <button>Join A Room</button>
+      <input
+        type="text"
+        placeholder="Hi Ahmed "
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Room ID ... "
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      />
+      <button onClick={joinRoom}>Join A Room</button>
+      <Chat socket={socket} userName={userName} room={room} />
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const app = express();
 const http = require('http');
 const cors = require('cors')
 
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
+const { Socket } = require('net');
 const server = http.createServer(app);
 
 const io = new Server(server ,{
@@ -15,6 +16,11 @@ const io = new Server(server ,{
 
 io.on("connection" , (socket) => {
   console.log(`user connection`,socket.id )
+
+  socket.on ("joinRoom" ,(data) => {
+    socket.join(data)
+    console.log(`user with id ${socket.id} join room: ${data}`);
+  })
 
   socket.on("disconnect" ,() => {
     console.log("user Disconnected" , socket.id);
